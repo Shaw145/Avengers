5c158c8d9264a25fb023113df01a360bdcf38b2e
SumanShaw <sumanshaw550@gmail.com>
2023-09-04 23:41:22 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,37 @@
+
+var crsr = document.querySelector('.cursor');
+var blur = document.querySelector('.cursor-blur');
+
+document.addEventListener("mousemove", function(dets){
+    crsr.style.left = dets.x+"px"
+    crsr.style.top = dets.y+"px"
+    blur.style.left = dets.x - 250 +"px"
+    blur.style.top = dets.y - 250 +"px"
+})
+
+
+gsap.to(".navbar", {
+    backgroundColor: "#000",
+    duration: 0.5,
+    height: "110px",
+    scrollTrigger: {
+      trigger: "#nav",
+      scroller: "body",
+      // markers:true,
+      start: "top -10%",
+      end: "top -11%",
+      scrub: 1,
+    },
+  });
+
+  gsap.to(".main-menu", {
+    backgroundColor: "#000",
+    scrollTrigger: {
+      trigger: "#main",
+      scroller: "body",
+      // markers: true,
+      start: "top -25%",
+      end: "top -70%",
+      scrub: 2,
+    },
+  });
